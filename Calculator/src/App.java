eb9af811048fa52069f3d5359475a910683f994d
JaySatoru <153070223+JaySatoru@users.noreply.github.com>
2024-08-24 19:26:26 +0530

    Add files via upload

---
+++ Calculator/src/App.java
@@ -0,0 +1,74 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class App extends JFrame implements ActionListener {
+
+    JTextArea display;
+    double num1, num2, result;
+    char operator;
+
+    public App() {
+        setLayout(new BorderLayout());
+
+        display = new JTextArea(2, 4);
+        display.setEditable(false);
+        add(display, BorderLayout.NORTH);
+
+        JPanel panel = new JPanel();
+        panel.setLayout(new GridLayout(4, 4));
+
+        String[] buttons = {
+            "7", "8", "9", "/",
+            "4", "5", "6", "*",
+            "1", "2", "3", "-",
+            "0", "C", "=", "+"
+        };
+
+        for (String text : buttons) {
+            JButton button = new JButton(text);
+            button.addActionListener(this);
+            panel.add(button);
+        }
+
+        add(panel, BorderLayout.CENTER);
+
+        setTitle("Calculator");
+        setSize(300, 400);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setVisible(true);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        String command = e.getActionCommand();
+
+        if (command.charAt(0) >= '0' && command.charAt(0) <= '9') {
+            display.setText(display.getText() + command);
+        } else if (command.charAt(0) == 'C') {
+            display.setText("");
+            num1 = num2 = result = 0;
+            operator = ' ';
+        } else if (command.charAt(0) == '=') {
+            num2 = Double.parseDouble(display.getText());
+            switch (operator) {
+                case '+': result = num1 + num2; break;
+                case '-': result = num1 - num2; break;
+                case '*': result = num1 * num2; break;
+                case '/': result = num1 / num2; break;
+            }
+            display.setText(String.valueOf(result));
+        } else {
+            if (!display.getText().isEmpty()) {
+                num1 = Double.parseDouble(display.getText());
+                operator = command.charAt(0);
+                display.setText("");
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        new App();
+    }
+}
